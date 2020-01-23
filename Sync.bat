a7716b03da8db63c55cd972ad1e2a1ac953b4b29
ITFS777 <1374924410@qq.com>
2020-01-23 15:31:49 +0800

    2020/01/23 15:31:49 by ITFS<Aryan.ITFS@Gmail.com>

--- Sync.bat
+++ Sync.bat
@@ -1,5 +1,5 @@
 @echo off
-xcopy C:\Users\13749\AppData\Roaming\Code\User\snippets D:\Github\ITFS777\VSCconfig\snippets /ey
+xcopy C:\Users\ITFS\AppData\Roaming\Code\User\snippets D:\Github\ITFS777\VSCconfig\snippets /ey
 git add .
 git commit -m "%date:~3,10% %time:~0,2%:%time:~3,2%:%time:~6,2% by ITFS<Aryan.ITFS@Gmail.com>"
 git push VSC master
